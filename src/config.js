2e4bef95edc1e3f48ee21f25fa8fcda3063dbd7d
Marco <mondial95@gmail.com>
2018-05-15 20:19:36 +0200

    🎨 🌊 moving global constants out of the shell

---
+++ src/config.js
@@ -0,0 +1,49 @@
+/**
+ * Application wide constants
+ *
+ */
+
+/**
+ * Api
+ * @type {Object}
+ */
+const API = {
+  /**
+   * @type {String}
+   */
+  HOST: 'http://localhost:8888/uni/radiator/ocean-eye-app/api/',
+}
+
+/**
+ * Page => router/navigation
+ * @type {Object}
+ */
+const PAGE = {
+  /**
+   * @type {String}
+   */
+  COLLECTIONS: 'collections',
+  /**
+   * @type {String}
+   */
+  PLAY: 'play',
+  /**
+   * @type {String}
+   */
+  PROFILE: 'profile',
+  /**
+   * @type {String}
+   */
+  LOGOUT: 'logout',
+}
+
+/**
+ * Events
+ * @type {Object}
+ */
+const CUSTOMEVENTS = {
+  /**
+   * @type {String}
+   */
+  CHANGE_PAGE: 'changepage',
+}
